2b4c945bb91c8c6adfa4c202d8dd1b1aed67e8c5
Ayush <ayushbisht750083@gmail.com>
2023-11-11 15:48:57 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+var audio=document.createElement('audio');
+audio.setAttribute('src','soundcar.mp3');
+
+// audio.setAttribute('autoplay','soundcar.mp3');
+audio.loop=true;
+audio.play();
+
+
+// let mySound = new Audio('soundcar.mp3')
+// mySound.play()
